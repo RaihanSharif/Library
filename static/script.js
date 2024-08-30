100d147afe13f7c9ed362a3b1c3ac684f29033e2
Raihan Sharif <raihansharif92@gmail.com>
2024-08-30 22:26:47 +0100

    Delete items from array and update table

--- static/script.js
+++ static/script.js
@@ -2,21 +2,21 @@ const lotr = {
     title: "Lord of the Rings",
     author: "J.R.R. Tolkien",
     pages: 600,
-    read: false,
+    isRead: false,
 }
 
 const mokingBird = {
     title: "To Kill a MockingBird",
     author: "Harper Lee",
     pages: 5000,
-    read: false,
+    isRead: false,
 }
 
 const shinning = {
     title: "The Shining",
     author: "Stephen King",
     pages: 300,
-    read: true,
+    isRead: true,
 }
 
 const myLibrary = [];
@@ -36,22 +36,59 @@ function addBookToLibrary() {
 
 
 
+
+const tableBody = document.querySelector(".books-table-body");
 function updateTable() {
-    const tableBody = document.querySelector(".books-table-body");
+    while (tableBody.hasChildNodes()) {
+        tableBody.removeChild(tableBody.lastChild);
+    }
     let tableRow = document.createElement("tr");
-    let td = document.createElement('td');
 
     for (let i = 0; i < myLibrary.length; i++) {
         tableRow = document.createElement("tr");
-        const temp = myLibrary[i];
+        const book = myLibrary[i];
 
-        for (let key in temp) {
-            td = document.createElement("td");
-            td.textContent = temp[key];
-            tableRow.appendChild(td);
+        const title = document.createElement("td");
+        title.textContent = book["title"];
+        
+        const author = document.createElement("td");
+        author.textContent = book["author"];
+        
+        const pages = document.createElement("td");
+        pages.textContent = book["pages"];
+        
+        const read = document.createElement("td");
+        read.textContent = book["isRead"];
+
+        const toggle_read = document.createElement("button");
+        toggle_read.textContent = "toggle";
+        read.appendChild(toggle_read);
+
+        const delBook = document.createElement("button");
+        delBook.textContent = "delete";
+        delBook.setAttribute('data', `del: ${i}`);
+        delBook.className = "delete-book";
+        delBook.onclick = () => {
+            delFromLibrary(delBook.dataset.del);
         }
+        
+        tableRow.appendChild(title);
+        tableRow.appendChild(author);
+        tableRow.appendChild(pages);
+        tableRow.appendChild(read);
+        tableRow.appendChild(delBook);
+
+
         tableBody.appendChild(tableRow);
     }
 }
 
 updateTable()
+
+function delFromLibrary(index) {
+    console.log(`before`, myLibrary);
+    myLibrary.splice(index, 1);
+    updateTable();
+    console.log(`after`, myLibrary);
+}
+
